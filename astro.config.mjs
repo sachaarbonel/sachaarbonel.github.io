4358b594d4acb916fb281c65ac2da61f809a95de
Yash Jawale <yash25.j@gmail.com>
2023-12-06 22:57:31 +0530

    Github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://yashjawale.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
