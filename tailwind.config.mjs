c738947d026f6345427b04312d8c48fec529ce8c
Yash Jawale <yash25.j@gmail.com>
2023-12-10 23:00:38 +0530

    Add coming soon page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#FFB800',
+				foreground: '#1F1F1F',
+				background: '#EFEFEF'
+			},
+			fontFamily: {
+				title: ["Silkscreen", ...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [],
 }
