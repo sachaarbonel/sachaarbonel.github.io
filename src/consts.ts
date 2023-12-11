23e50c091a00411b03c339efe36e392bc092cf1c
Yash Jawale <yash25.j@gmail.com>
2023-12-11 23:03:47 +0530

    Update tool links

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Yash Jawale';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Yash\'s Portfolio Website';
