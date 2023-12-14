d0f182a368ba7b9136eaadead397912c6b77b8cc
Yash Jawale <yash25.j@gmail.com>
2023-12-14 09:52:30 +0530

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),]
 });
