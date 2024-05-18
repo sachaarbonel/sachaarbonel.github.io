ee64e62116c2f5e781483f4d035e6061cfb7b548
yashjawale <yash25.j@gmail.com>
2024-05-18 23:27:31 +0530

    Update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yashjawale.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), partytown({
+  integrations: [mdx(), sitemap(), tailwind(), icon(), partytown({
     config: {
       forward: ["dataLayer.push"],
     },
