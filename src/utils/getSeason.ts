4c062778ba1537838cf583c3525027bc9443c222
yashjawale <yash25.j@gmail.com>
2024-09-09 23:31:49 +0530

    Enable seasons toggle

--- src/utils/getSeason.ts
+++ src/utils/getSeason.ts
@@ -7,13 +7,12 @@
 
 // Return the current season
 function getSeason() {
-  // const month = new Date().getMonth() + 1
-  // if (month <= 2) return 'spring'
-  // if (month >= 3 && month <= 5) return 'summer'
-  // if (month >= 6 && month <= 9) return 'monsoon'
-  // if (month == 10) return 'autumn'
-  // return 'winter'
-  return 'autumn'
+  const month = new Date().getMonth() + 1
+  if (month <= 2) return 'spring'
+  if (month >= 3 && month <= 5) return 'summer'
+  if (month >= 6 && month <= 9) return 'monsoon'
+  if (month == 10) return 'autumn'
+  return 'winter'
 }
 
 export default getSeason
