44e9b1a8ca99f755c9825079d5799161855ac04f
yashjawale <yash25.j@gmail.com>
2024-09-09 23:18:41 +0530

    Add all seasons

---
+++ src/utils/getSeason.ts
@@ -0,0 +1,19 @@
+//   Season	Timeline
+// Spring	Jan-Feb
+// Summer	Mar-May
+// Monsoon	Jun-Sep
+// Autumn	Oct
+// Winter	Nov-Dec
+
+// Return the current season
+function getSeason() {
+  // const month = new Date().getMonth() + 1
+  // if (month <= 2) return 'spring'
+  // if (month >= 3 && month <= 5) return 'summer'
+  // if (month >= 6 && month <= 9) return 'monsoon'
+  // if (month == 10) return 'autumn'
+  // return 'winter'
+  return 'autumn'
+}
+
+export default getSeason
