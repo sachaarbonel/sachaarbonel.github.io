2b2b5c3ad2e91bc706e376d54f5cee39bacde18b
yashjawale <yash25.j@gmail.com>
2024-10-24 22:57:07 +0530

    Complete homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,47 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yash Jawale';
-export const SITE_DESCRIPTION = 'Yash\'s Portfolio Website';
+export const SITE_TITLE = 'Yash Jawale'
+export const SITE_DESCRIPTION = "Yash's Portfolio Website"
+
+export const SocialLinks = [
+  {
+    name: 'Github',
+    url: 'https://github.com/yashjawale',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/yashjawale/',
+  },
+  {
+    name: 'X/Twitter',
+    url: 'https://twitter.com/imyashjawale',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/imyashjawale',
+  },
+]
+
+export const WebsiteLinks = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+  {
+    name: 'Blog',
+    url: '/blog',
+  },
+  {
+    name: 'Projects',
+    url: '/projects',
+  },
+  {
+    name: 'Playground',
+    url: '/playground',
+  },
+  {
+    name: 'Contact',
+    url: 'mailto:yash25.j@gmail.com',
+  },
+]
