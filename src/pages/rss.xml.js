71582bfb45a873ad7897bf6182df6f1673793faf
yashjawale <yash25.j@gmail.com>
2025-09-13 16:49:12 +0530

    fix: remove content from RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,6 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
-import sanitizeHtml from 'sanitize-html'
-import MarkdownIt from 'markdown-it'
-const parser = new MarkdownIt()
-
 export async function GET(context) {
 	const blog = await getCollection('blog')
 	return rss({
@@ -24,10 +20,7 @@ export async function GET(context) {
 			description: post.data.description,
 			// Compute RSS link from post `id`
 			// This example assumes all posts are rendered as `/blog/[id]` routes
-			link: `/blog/${post.id}/`,
-			content: sanitizeHtml(parser.render(post.body), {
-				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-			}),
+			link: `/blog/${post.id}/`
 		})),
 	})
 }
